4dd6748e3c82767ed1505c760151554b3eedd14d
Kit Rana <50119092+KitRana@users.noreply.github.com>
2019-10-08 18:58:13 -0400

    added nycw app to heroku

---
+++ app.py
@@ -0,0 +1,38 @@
+
+from flask import Flask, render_template, redirect
+import crime_web_scraping
+from config import password
+from flask_pymongo import PyMongo
+from pymongo import MongoClient
+import pymongo
+
+
+app = Flask(__name__)
+app.config["MONGO_URI"] = f'mongodb+srv://kitrana08:{password}@nycw-azcb7.mongodb.net/test?retryWrites=true&w=majority'
+mongo = PyMongo(app)
+
+
+@app.route("/")
+def index():
+    crime_data = crime_web_scraping.scrape()
+    mongo.db.crime.replace_one({}, crime_data, True)
+    crime = mongo.db.crime.find_one()
+    return render_template("index.html", crime=crime)
+
+# Chart 
+@app.route("/chart")
+def chart ():
+    return render_template("chart.html")
+
+# Map
+@app.route("/map")
+def fullmap ():
+    return render_template("map.html")
+
+# about
+@app.route("/documentation")
+def about ():
+    return render_template("about.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
